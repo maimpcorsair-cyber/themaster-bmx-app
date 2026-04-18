0882c21a0ad6d8e51e42bca1cdca09860e1c50a4
Chuvit <chuvit@Chuvits-Mac-mini.local>
2026-04-18 11:04:20 +0700

    Initial React + Capacitor app for APK build

---
+++ android/app/src/main/java/com/themaster/bmx/MainActivity.java
@@ -0,0 +1,5 @@
+package com.themaster.bmx;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.themaster.bmx',
+  appName: 'TheMasterBMX',
+  webDir: 'build'
+};
+
+export default config;
